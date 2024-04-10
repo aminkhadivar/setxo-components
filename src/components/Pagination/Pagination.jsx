99471ab799943f02edb1ca6e9c41120dda0d0a24
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-04-10 22:18:57 +0330

    Create Pagination component

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,66 @@
+import { createContext } from 'react'
+import { Link } from '@inertiajs/react'
+import './Pagination.css'
+
+const PaginationContext = createContext()
+
+const Pagination = ({ children, className = '' }) => {
+    return (
+        <PaginationContext.Provider value={{ className, children }}>
+            <nav aria-label="navigation">
+                <ul className={'pagination' + (className && ` ${className}`)}>
+                    {children}
+                </ul>
+            </nav>
+        </PaginationContext.Provider>
+    );
+};
+
+const PaginationItem = ({ children, className = '', active = '', rounded = 'rounded', disabled = '', size = 'default', href, ...props }) => {
+
+    const roundedClass = {
+        none: 'rounded-none',
+        rounded: 'rounded',
+        sm: 'rounded-sm',
+        md: 'rounded-md',
+        lg: 'rounded-lg',
+        full: 'rounded-full',
+    }[rounded];
+
+    const sizeClass = {
+        sm: 'text-sm px-3 h-8',
+        md: 'px-3 h-9',
+        default: 'px-4 h-10',
+        lg: 'text-lg px-4 h-11',
+        xl: 'text-xl px-5 h-12',
+    }[size];
+
+    return (
+        <li
+            {...props}
+            className={'page-item' + `${active && ' active'}` + `${disabled && ' disabled'}`}
+        >
+            {href
+                ?
+                <>
+                    <Link
+                        className={`page-link ${sizeClass} ` + className + roundedClass}
+                        href={href}
+                    >
+                        {children}
+                    </Link>
+                </>
+                :
+                <>
+                    <div className={`page-link ${sizeClass} ` + className + roundedClass}>
+                        {children}
+                    </div>
+                </>
+            }
+        </li>
+    );
+};
+
+Pagination.Item = PaginationItem;
+
+export default Pagination;
