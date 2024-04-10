76506d6b4b41286463220d52e356da2a3274e50e
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-04-10 21:27:22 +0330

    Create button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,90 @@
+import { Link } from "@inertiajs/react"
+import './Button.css'
+
+export default function Button({ href, as = 'Link', type = 'button', className = '', disabled = '', size = 'default', color = 'base', borderColor = '', children, rounded = 'rounded', ...props }) {
+
+    const colorClass = {
+        base: 'btn',
+        default: 'btn btn-default',
+        light: 'btn btn-light',
+        gray: 'btn btn-gray',
+        dark: 'btn btn-dark',
+        primary: 'btn btn-primary',
+        success: 'btn btn-success',
+        danger: 'btn btn-danger',
+        warning: 'btn btn-warning',
+        info: 'btn btn-info',
+        purple: 'btn btn-purple',
+    }[color];
+
+    const borderColorClass = {
+        default: ' btn-outline-default',
+        light: ' btn-outline-light',
+        gray: ' btn-outline-gray',
+        dark: ' btn-outline-dark',
+        primary: ' btn-outline-primary',
+        success: ' btn-outline-success',
+        danger: ' btn-outline-danger',
+        warning: ' btn-outline-warning',
+        info: ' btn-outline-info',
+        purple: ' btn-outline-purple',
+    }[borderColor];
+
+    const sizeClass = {
+        sm: 'text-sm h-8',
+        md: 'h-9',
+        default: 'h-10',
+        lg: 'text-lg h-11',
+        xl: 'text-xl h-12',
+    }[size];
+
+    const roundedClass = {
+        none: 'rounded-none',
+        rounded: 'rounded',
+        sm: 'rounded-sm',
+        md: 'rounded-md',
+        lg: 'rounded-lg',
+        full: 'rounded-full',
+    }[rounded];
+
+    return (
+        href
+            ?
+            <>
+                {as === 'Link' &&
+                    <Link
+                        {...props}
+                        href={href}
+                        role="button"
+                        className={`${colorClass} px-2 ` + className + `${disabled && ' pointer-events-none opacity-50'
+                            }` + ` ${rounded && roundedClass}` + ` ${sizeClass}` + `${borderColor ? borderColorClass : ''}`}
+                    >
+                        {children}
+                    </Link>
+                }
+                {as === 'externalLink' &&
+                    <a
+                        {...props}
+                        href={href}
+                        role="button"
+                        target="_blank"
+                        
+                        rel="noreferrer"
+                        className={`${colorClass} px-2 ` + className + `${disabled && ' pointer-events-none opacity-50'
+                            }` + ` ${rounded && roundedClass}` + ` ${sizeClass}` + `${borderColor ? borderColorClass : ''}`}
+                    >
+                        {children}
+                    </a>
+                }
+            </>
+            :
+            <button
+                {...props}
+                type={type}
+                className={`${colorClass} px-2 ` + className + `${disabled && ' pointer-events-none opacity-50'
+                    }` + ` ${rounded && roundedClass}` + ` ${sizeClass}` + `${borderColor ? borderColorClass : ''}`}
+            >
+                {children}
+            </button>
+    )
+}
