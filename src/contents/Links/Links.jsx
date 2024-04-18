57ac3edf0c936298e6c511557ba9251fdae25e81
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-04-18 11:51:01 +0330

    Add Links component

---
+++ src/contents/Links/Links.jsx
@@ -0,0 +1,42 @@
+import { Link } from "@inertiajs/react"
+import './Links.css'
+
+export default function A({ href, as = 'Link', className = '', color = 'primary', underline = '', underlineOnHover = '', children, ...props }) {
+
+    const colorClass = {
+        light: 'link-light',
+        gray: 'link-gray',
+        dark: 'link-dark',
+        primary: 'link-primary',
+        success: 'link-success',
+        danger: 'link-danger',
+        warning: 'link-warning',
+        info: 'link-info',
+        purple: 'link-purple',
+    }[color];
+
+    return (
+        <>
+            {as === 'Link' &&
+                <Link
+                    {...props}
+                    href={href}
+                    className={`${colorClass}` + `${underline && ' underline'}` + `${underlineOnHover && ' hover:underline'}` + className}
+                >
+                    {children}
+                </Link>
+            }
+            {as === 'externalLink' &&
+                <a
+                    {...props}
+                    href={href}
+                    target="_blank"
+                    rel="noreferrer"
+                    className={`${colorClass}` + `${underline && ' underline'}` + className}
+                >
+                    {children}
+                </a>
+            }
+        </>
+    )
+}
