e55c13074ec4b9e35666d5bd8f7490b8bcb0eb65
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-04-18 11:48:27 +0330

    Delete src/Forms directory

--- src/Forms/InputHelp.jsx
+++
@@ -1,7 +0,0 @@
-export default function InputError({ message, className = '', ...props }) {
-    return message ? (
-        <p {...props} className={'text-sm text-gray-400 dark:text-gray-500 ' + className}>
-            {message}
-        </p>
-    ) : null;
-}
