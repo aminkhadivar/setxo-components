fdb1b74a2e7905878256400f74b4d54864e390a2
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-05-28 02:38:45 +0330

    Update Avatar.jsx

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,11 +1,11 @@
 import Image from "../../contents/Image/Image"
 import './Avatar.css'
 
-export default function Avatar({ children, url, alt, as = 'image', size, height = '48', className = '', rounded = 'rounded' , border = '', ...props }) {
+export default function Avatar({ children, url, alt, size, height = '48', className = '', rounded = 'rounded', border = '', ...props }) {
 
     const DefaultAvatar = () => {
         return (
-            <svg className="fill-gray-400" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <svg className="fill-gray-400" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22.0508 26C22.0508 23.3478 20.9972 20.8043 19.1218 18.9289C17.2465 17.0536 14.7029 16 12.0508 16C9.39862 16 6.85508 17.0536 4.97971 18.9289C3.10435 20.8043 2.05078 23.3478 2.05078 26L12.0508 26H22.0508Z" />
                 <path d="M17 9C17 11.7614 14.7614 14 12 14C9.23858 14 7 11.7614 7 9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9Z" />
             </svg>
