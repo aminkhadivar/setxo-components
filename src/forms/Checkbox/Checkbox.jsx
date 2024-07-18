f091891f3a4be03eeae90aa92747704e3794944d
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-07-18 17:44:51 +0330

    Update checkbox

--- src/forms/Checkbox/Checkbox.jsx
+++ src/forms/Checkbox/Checkbox.jsx
@@ -1,15 +1,21 @@
+import { forwardRef, useRef, useState } from 'react'
+import Label from "../FormControl/Label"
 import './Checkbox.css'
 export default function Checkbox({ id, className = '', label, disabled = '', defaultChecked, ...props }) {
     return (
-        <input
-            {...props}
-            type="checkbox"
-            id={id}
-            className={`form-check-input` +
-                `${className && ` ` + className}` + `${disabled ? ' pointer-events-none opacity-50' : ' cursor-pointer'}`
-            }
-            defaultChecked={defaultChecked}
-            disabled={disabled}
-        />
+        <div className="form-check">
+            <Label htmlFor={id} value={label}>
+                <input
+                    {...props}
+                    type="checkbox"
+                    id={id}
+                    className={`form-check-input` +
+                        `${className && ` ` + className}` + `${disabled ? ' pointer-events-none opacity-50' : ' cursor-pointer'}`
+                    }
+                    defaultChecked={defaultChecked}
+                    disabled={disabled}
+                />
+            </Label>
+        </div>
     )
 }
