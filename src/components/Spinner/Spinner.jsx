454423378beead6fd4b885ebf36f7fc6e9171a16
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-07-24 13:53:55 +0330

    Add Spinner component

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,36 @@
+
+import './Spinner.css'
+
+export default function Spinner({ as = 'border', className = '', color = '', size = '', ...props }) {
+
+    const colorClass = {
+        default: 'spinner-default',
+        light: 'spinner-light',
+        gray: 'spinner-gray',
+        dark: 'spinner-dark',
+        primary: 'spinner-primary',
+        success: 'spinner-success',
+        danger: 'spinner-danger',
+        warning: 'spinner-warning',
+        info: 'spinner-info',
+        purple: 'spinner-purple',
+        custom: ''
+    }[color]
+
+    const sizeClass = {
+        sm: 'spinner-sm',
+        lg: 'spinner-lg',
+    }[size]
+
+    return (
+        <>
+            <div
+                {...props}
+                className={`${as === 'border' ? 'spinner-border' : 'spinner-grow'}` + `${className && ` ` + className}` + `${color ? ` ` + colorClass : ' spinner-default'}` + `${size && ` ` + sizeClass}`}
+                role="status"
+            >
+                <span className="hidden">Loading...</span>
+            </div>
+        </>
+    )
+}
