491d2eb4f1c66bc3e69722feb413f7fa7f07efef
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-09-12 00:07:19 +0330

    Update Range
    Add range thumb color

--- src/forms/Range/Range.jsx
+++ src/forms/Range/Range.jsx
@@ -26,6 +26,20 @@ export default function Range({ max = '100', min = '0', step, value, className =
         custom: ''
     }[color]
 
+    const thumbColorClass = {
+        default: '',
+        light: 'form-range-input-light',
+        gray: 'form-range-input-gray',
+        dark: 'form-range-input-dark',
+        primary: 'form-range-input-primary',
+        success: 'form-range-input-success',
+        danger: 'form-range-input-danger',
+        warning: 'form-range-input-warning',
+        info: 'form-range-input-info',
+        purple: 'form-range-input-purple',
+        custom: ''
+    }[color]
+
     const sizeClass = {
         sm: 'range-sm',
         lg: 'range-lg',
@@ -44,7 +58,7 @@ export default function Range({ max = '100', min = '0', step, value, className =
                         max={max}
                         step={step}
                         onChange={onChangeInput}
-                        className={`form-range-input`}
+                        className={`form-range-input`+ `${color && ` ` + thumbColorClass}`}
                     />
                     
                     <div className={`form-range-fill` + `${className && ` ` + className}` + `${color ? ` ` + colorClass : ' range-default'}`} style={{ width: `${percent}%` }}></div>
